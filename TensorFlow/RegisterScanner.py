76056e881035634cf6ce7fb78747b519ee69f564
Mike <msfurr94@gmail.com>
2019-10-14 10:23:37 -0400

    commit

--- TensorFlow/RegisterScanner.py
+++ TensorFlow/RegisterScanner.py
@@ -2,8 +2,8 @@ from periphery import I2C
 
 i2c = I2C('/dev/i2c-1')
 
-for i in range(80, 90):
+for i in range(128, 138):
 
-	msgs = [I2C.Message([0xi]), I2C.Message([0x00], read=True)]
+	msgs = [I2C.Message([i]), I2C.Message([0x00], read=True)]
 	i2c.transfer(0x13, msgs)
 	print(msgs[1].data)
