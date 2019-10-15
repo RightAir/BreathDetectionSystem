4ac814fe23a512b8e19b3d6c39408e95a596a696
Mike <msfurr94@gmail.com>
2019-10-15 11:45:09 -0400

    Update DataGathering.py

--- TensorFlow/DataGathering.py
+++ TensorFlow/DataGathering.py
@@ -4,12 +4,7 @@ import time
 Proximity = Proximity_Sensor.VCNL4010()
 
 Proximity_Data = []
-try:
-	print('Ctrl-C to end')
-	while(True):
-		Proximity_Data.append(Proximity.proximity)
-		print(Proximity.proximity)
-		time.sleep(1)
-	except KeyboardInterrupt, e:
-		print('Stopped')
-		raise
+while(True):
+	Proximity_Data.append(Proximity.proximity)
+	print(Proximity.proximity)
+	time.sleep(1)
