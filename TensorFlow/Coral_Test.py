fcecae559fe6576b662ba40246528ebc6bce99f9
Mike <msfurr94@gmail.com>
2019-10-15 12:00:59 -0400

    Coral Inference Updates

--- TensorFlow/Coral_Test.py
+++ TensorFlow/Coral_Test.py
@@ -70,27 +70,24 @@ def classSwitch(Class):
 
 def main():
 
+    # Gather data from text file
     data = np.loadtxt('test1.txt')
     data = np.float32([data])
     
+    # Setup interpreter for inference
     interpreter = Interpreter(model_path="my_tflite_model_4.tflite")
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()
     output_details = interpreter.get_output_details()
     
-#    input_shape = input_details[0]['shape']
-#    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-    
-#    interpreter.set_tensor(input_details[0]['index'], input_data)
-#    interpreter.invoke()
-#    print(interpreter.get_tensor(output_details[0]['index']))
-    
     start = time.time()
+    
+    results= []
     for i in range(0, len(data[0])):
         input_data = data[0][[i]]
         interpreter.set_tensor(input_details[0]['index'], input_data)
         interpreter.invoke()
-        print(interpreter.get_tensor(output_details[0]['index']))
+        results.append(interpreter.get_tensor(output_details[0]['index']))
     
     duration = time.time() - start
     print(duration / len(data[0]))
